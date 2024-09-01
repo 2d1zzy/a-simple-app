79b5640537f3c05ad3cc71103d56db3b9511ff09
2d1zzy <168884170+2d1zzy@users.noreply.github.com>
2024-09-01 09:20:51 +0700

    Added the enter key for the text.

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var main = function () {
     $(".comment-input button").on("click", function (event){
         var $new_comment;
 
-        if ($(".comment-input input").val() !== ""){
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $(".comments").append($new_comment);
-            $(".comment-input input").val("");
+        if (event.keyCode === 13) {  
+            if ($(".comment-input input").val() !== ""){
+                $new_comment = $("<p>").text($(".comment-input input").val());
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("");
             
-         }
+            }
+        }      
     });
 }
 
